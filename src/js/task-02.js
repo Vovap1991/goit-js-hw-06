2cebdb022e3e4bc1976208c9994490f90f3ae968
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-05-31 22:07:22 +0300

    2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,48 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Варіант 1
+
+const newArr = ingredients.map(element => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = element;
+  itemEl.classList.add('item');
+  return itemEl;
+  newArr.push(itemEl);
+})
+
+  const listEl = document.querySelector('#ingredients');
+  listEl.append(...newArr);
+
+
+
+  
+  // Варіант 2
+
+// const newArr = [];
+
+// ingredients.forEach(element => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = element;
+//   itemEl.classList.add('item');
+//   newArr.push(itemEl);
+// })
+
+//   const listEl = document.querySelector('#ingredients');
+//   listEl.append(...newArr);
+
+
+
+
+
+// Варіант 3
+
+// const listEl = document.querySelector('#ingredients');
+
+// ingredients.forEach(element => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = element;
+//   itemEl.classList.add('item');
+//   listEl.appendChild(itemEl);
+// })
