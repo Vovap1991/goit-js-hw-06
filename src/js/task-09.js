e75670762f263b8584a5fa1132addd0b6a877902
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-05 21:15:11 +0300

    9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const bttnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+bttnEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+} );
