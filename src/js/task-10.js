0f79c40626832d8607cabd9825ce097f87f2eb3d
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-05 22:30:38 +0300

    10 done?

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+const mainDivEl = document.querySelector('#boxes');
+
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = inputEl.value;
+  let startSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement('div');
+    startSize += 10;
+    newBox.style.width = startSize + 'px';
+    newBox.style.height = startSize + 'px';
+    newBox.style.backgroundColor = getRandomHexColor();
+    mainDivEl.appendChild(newBox);
+  }
+}
+
+function destroyBoxes() {
+  mainDivEl.innerHTML = "";
+  inputEl.value = "";
+}
