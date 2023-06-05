d085dd8892712dea3725e38f7d3be3c0a87e4672
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-05 20:35:35 +0300

    8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const loginFormEl = document.querySelector('.login-form');
+const emailInputEl = loginFormEl.elements.email;
+const passwordInputEl = loginFormEl.elements.password;
 
+loginFormEl.addEventListener("submit", onClick);
+
+function onClick(e) {
+    e.preventDefault();
+
+    const {
+        elements: { email, password } 
+    } = e.currentTarget;
+
+    if (email.value === "" || password.value === "")  {
+       return alert(`Всі поля мають бути заповнені!`);
+    } 
+
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(formData);
+    e.currentTarget.reset();
+}
